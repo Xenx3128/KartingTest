0ea7c2e49c64023039946fdcf4a183d93f430df4
Xenx3128 <Xenx3024@yandex.ru>
2025-04-14 08:12:45 +0500

    add account design

---
+++ Pages/Account/History.cshtml.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+using TestMVC.Data;
+using TestMVC.Utility;
+using TestMVC.Models;
+
+namespace TestMVC.Pages;
+public class AccHistoryModel : PageModel
+{
+    private string connectionString = ConnectionString.CName;
+    private readonly UserContext _userContext;
+
+    public AccHistoryModel()
+    {
+        _userContext = new UserContext(connectionString);
+    }
+
+    [BindProperty]
+    public string Email { get; set; }
+
+    [BindProperty]
+    public string Password { get; set; }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var user = await _userContext.GetUserByEmail(Email);
+
+        if (user != null && user.Pwd == Password)
+        {
+            // Successful login logic here
+            return RedirectToPage("/Index");
+        }
+
+        ModelState.AddModelError("", "Invalid email or password.");
+        return Page();
+    }
+}
---
+++ wwwroot/js/history.js
@@ -0,0 +1,10 @@
+new DataTable('#raceHistory', {
+    language: {
+        info: 'Заезд _START_-_END_ из _TOTAL_',
+        infoEmpty: 'Тут пока пусто',
+        infoFiltered: '(отфильтровано из _MAX_ заездов)',
+        lengthMenu: 'Показывать _MENU_ заездов на странице',
+        search: "Поиск:",
+        zeroRecords: 'Ничего не найдено - извините'
+    }
+});
